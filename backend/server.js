1b5c221c5af765d9822adbcae86f90b45a064a8c
Mohan <mohanpraveen4161@gmail.com>
2024-09-16 22:10:26 +0530

    third

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,10 @@ app.post('/verify-payment', (req, res) => {
   }
 });
   
-  
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 
 app.listen(5000, () => {
   console.log('Server started on port 5000');
